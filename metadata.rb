52bc4c139906f10d8c9c46cd8f6461d7c0fe7c7c
John Sweitzer <john_sweitzer@thirdwaveinsights.com>
2016-04-28 15:09:51 -0500

    Change v.r.m to 0.1.1

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name 'topology-truck'
 maintainer 'John Sweitzer'
 maintainer_email 'john_sweitzer@thirdwaveinsights.com'
 license 'all_rights'
-version '0.1.0'
+version '0.1.1'
 issues_url 'https://github.com/john_sweitzer/topology-truck/issues'
 source_url 'https://github.com/john_sweitzer/topology-truck'
 
