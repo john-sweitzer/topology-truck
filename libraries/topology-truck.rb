50528e175fa224a2b7c5366a608bfebdb4709280
John Sweitzer <john_sweitzer@thirdwaveinsights.com>
2016-05-09 15:32:58 -0500

    Use 'stage' instead of 'stages'

--- libraries/topology-truck.rb
+++ libraries/topology-truck.rb
@@ -67,11 +67,11 @@ class Topo
       
       # Do we have stages detail...
       if @raw_data['stages']
-          stage_details = @raw_data['stages'] || {} 
-          @acceptance_topologies    = stage_details['acceptance']['topologies'] || [] if stages_details['acceptance']
-          @union_topologies         = stage_details['union']['topologies'] || [] if stages_details['union']
-          @rehersal_topologies      = stage_details['rehersal']['topologies'] || [] if stages_details['rehearsal']
-          @delivered_topologies     = stage_details['delivered']['topologies'] || [] if stages_details['delivered']
+          stage_details = @raw_data['stages'] || {}
+          @acceptance_topologies    = stage_details['acceptance']['topologies'] || [] if stage_details['acceptance']
+          @union_topologies         = stage_details['union']['topologies'] || [] if stage_details['union']
+          @rehersal_topologies      = stage_details['rehersal']['topologies'] || [] if stage_details['rehearsal']
+          @delivered_topologies     = stage_details['delivered']['topologies'] || [] if stage_details['delivered']
       end
 
 
