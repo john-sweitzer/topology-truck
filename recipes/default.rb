6b2175eb830c19d03741187c747852e64d4e8ce0
John Sweitzer <john_sweitzer@thirdwaveinsights.com>
2016-05-09 10:23:20 -0500

    Fixed syntax error due to extra {

--- recipes/default.rb
+++ recipes/default.rb
@@ -19,7 +19,7 @@ raw_data['topology-truck'] = node['delivery']['config']['topology-truck']
 topo_truck_parms = Topo::ConfigurationParameter.new(raw_data.to_hash,stage) if raw_data['topology-truck']
 
 unsupportedDriver = topo_truck_parms.driver() == 'aws' || topo_truck_parms.driver() == 'ssh'
-raise ArgumentError, " '#{{topo_truck_parms.driver()}' is not a supported Chef provisioning driver at this time. Try using 'ssh' or 'aws' " if unsupportedDriver
+raise ArgumentError, " '#{topo_truck_parms.driver()}' is not a supported Chef provisioning driver at this time. Try using 'ssh' or 'aws' " if unsupportedDriver
 
 
 
